d9178b6c846e579b3547cc59e9ab47240340683a
yuzu <1581523609@qq.com>
2024-10-10 17:16:27 +0800

    feat: 在 Pinia 配置中添加新模块导出
    此更新在 Pinia 配置文件中添加了多个新模块的导出：
    - user 用户模块
    - tag 标签模块
    - permission 权限模块
    - loading 加载模块
    - dict 字典模块
    
    这些模块可能包含应用的状态管理和相关逻辑。导出它们使得可以在其他地方方便地引用和使用这些模块的功能。

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -4,4 +4,10 @@ import { createPersistedState } from 'pinia-plugin-persistedstate'
 const pinia = createPinia()
 pinia.use(createPersistedState())
 
+export * from './modules/user'
+export * from './modules/tag'
+export * from './modules/permission'
+export * from './modules/loading'
+export * from './modules/dict'
+
 export default pinia
