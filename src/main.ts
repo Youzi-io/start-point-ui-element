aa14377c707b5e5ba79a8e731e866dc266d3987a
yuzu <1581523609@qq.com>
2024-10-15 17:32:47 +0800

    feat: 在项目中集成国际化(i18n)支持
    本次更新引入了

--- src/main.ts
+++ src/main.ts
@@ -15,10 +15,12 @@ import 'nprogress/nprogress.css' // 进度条
  * 核心
  */
 import './permission'
+import i18n from './locales'
 
 const app = createApp(App)
 
 app.use(pinia)
+app.use(i18n)
 app.use(router)
 
 app.mount('#app')
