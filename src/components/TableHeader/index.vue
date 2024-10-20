91893ae1088d5228066a6158795f94071c0c9879
yuzu <1581523609@qq.com>
2024-10-20 11:23:57 +0800

    feat(components): 添加表格头组件
    - 新增 TableHeader 组件，包含刷新、添加、编辑和删除功能按钮
    - 使用 Element UI 和自定义 MSIcon 组件
    - 通过 props 控制编辑和删除按钮的禁用状态
    - 发送自定义事件以处理按钮点击

---
+++ src/components/TableHeader/index.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="table-header">
+    <el-tooltip placement="top" trigger="hover" content="刷新">
+      <el-button type="primary" @click="refreshClick">
+        <template #icon>
+          <MSIcon name="Cached" size="20"></MSIcon>
+        </template>
+      </el-button>
+    </el-tooltip>
+
+    <el-tooltip placement="top" trigger="hover" content="添加">
+      <el-button type="primary" style="margin: 0 0 0 8px" @click="addClick">
+        <template #icon>
+          <MSIcon name="Add" size="20"></MSIcon>
+        </template>
+        <span> 添加 </span>
+      </el-button>
+    </el-tooltip>
+
+    <el-tooltip placement="top" trigger="hover" content="编辑选中行">
+      <el-button type="info" :disabled="props.isNoSelection" style="margin: 0 0 0 8px" @click="editClick">
+        <template #icon>
+          <MSIcon name="Edit" size="20"></MSIcon>
+        </template>
+        <span> 编辑 </span>
+      </el-button>
+    </el-tooltip>
+
+    <el-tooltip placement="top" trigger="hover" content="删除选中行">
+      <el-popconfirm @confirm="deleteClick" title="确定删除选中记录？">
+        <template #reference>
+          <el-button type="danger" :disabled="props.isNoSelection" style="margin: 0 0 0 8px">
+            <template #icon>
+              <MSIcon name="Delete" size="20"></MSIcon>
+            </template>
+            <span> 删除 </span>
+          </el-button>
+        </template>
+      </el-popconfirm>
+    </el-tooltip>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import MSIcon from '@/components/MSIcon/index.vue'
+
+const props = withDefaults(defineProps<Props>(), {
+  isNoSelection: true
+})
+const emit = defineEmits(['refreshClick', 'addClick', 'editClick', 'deleteClick'])
+
+const refreshClick = () => {
+  emit('refreshClick')
+}
+const addClick = () => {
+  emit('addClick')
+}
+
+const editClick = () => {
+  emit('editClick')
+}
+const deleteClick = () => {
+  emit('deleteClick')
+}
+</script>
+<script lang="ts">
+interface Props {
+  isNoSelection: boolean
+}
+</script>
+
+<style lang="scss" scoped>
+.table-header {
+  margin: 10px;
+}
+</style>
