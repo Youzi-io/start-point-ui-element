bc3a60ba92b5fc2742284027e26157127367e015
Yuzu <1581523609@qq.com>
2024-10-21 17:24:01 +0800

    feat(system): 添加字典数据管理功能
    - 新增字典数据添加、编辑和批量编辑功能
    - 实现字典数据列表展示和筛选功能
    - 集成状态标签和操作按钮
    - 优化用户交互和数据加载逻辑

---
+++ src/views/system/dictData/batchEdit/index.vue
@@ -0,0 +1,162 @@
+<template>
+  <el-dialog v-model="show" title="编辑字典数据" width="500">
+    <el-form ref="formRef" :model="formData" :rules="rules" label-position="left" label-width="auto">
+      <el-form-item label="字典类型" prop="dictType">
+        <el-input v-model="formData.dictType" disabled placeholder="请输入字典类型" />
+      </el-form-item>
+
+      <el-form-item label="字典标签" prop="dictTag">
+        <el-input v-model="formData.dictTag" placeholder="请输入字典标签" />
+      </el-form-item>
+
+      <el-form-item label="字典键值" prop="dictValue">
+        <el-input v-model="formData.dictValue" placeholder="请输入字典键值" />
+      </el-form-item>
+
+      <el-form-item label="字典排序" prop="dictOrder">
+        <el-input-number v-model="formData.dictOrder" :min="0" :max="100" placeholder="请输入排序" />
+      </el-form-item>
+
+      <el-form-item label="回显样式" prop="listClass">
+        <el-select v-model="formData.listClass" placeholder="请选择回显样式" style="width: 140px">
+          <el-option v-for="item in listClassOptions" :key="item.id" :label="item.dictTag" :value="item.dictValue" />
+        </el-select>
+      </el-form-item>
+
+      <el-form-item label="状态" prop="status">
+        <el-radio-group v-model="formData.status">
+          <el-radio v-for="item in statusOptions" :key="item.id" :value="item.dictValue">{{
+            item.dictTag
+          }}</el-radio>
+        </el-radio-group>
+      </el-form-item>
+
+      <el-form-item label="备注" prop="remark">
+        <el-input v-model="formData.remark" type="textarea" placeholder="请输入备注" />
+      </el-form-item>
+    </el-form>
+    <template #footer>
+      <el-button @click="show = false">取消</el-button>
+      <el-button type="primary" @click="onSubmit(formRef)"> {{ submitText }} </el-button>
+    </template>
+  </el-dialog>
+</template>
+
+<script setup lang="ts">
+import { reactive, ref, watch } from 'vue'
+import type { DictDataInfo, EditDictDataParams } from '@/types/system/dictData'
+import { editDictDataApi, findByIdApi } from '@/api/system/dictData'
+import { useDictStore } from '@/stores'
+import { ElMessage, type FormInstance, type FormRules } from 'element-plus';
+
+const show = ref(false)
+const isMultiple = ref(false)
+const currentIndex = ref(0)
+const idsList = ref<string[]>([])
+const submitText = ref('保存')
+
+watch(
+  () => currentIndex.value,
+  () => {
+    isMultiple.value = idsList.value.length - 1 === currentIndex.value
+    submitText.value = isMultiple.value ? '保存' : '保存并编辑下一页'
+  }
+)
+
+const formRef = ref()
+const formData = ref<EditDictDataParams>({
+  id: '',
+  dictType: '',
+  dictTag: '',
+  dictValue: '',
+  dictOrder: 1,
+  listClass: 'info',
+  status: '0',
+  remark: ''
+})
+const rules = reactive<FormRules<EditDictDataParams>>({
+  dictTag: [{ required: true, message: '字典标签不能为空', trigger: 'blur' }],
+  dictValue: [{ required: true, message: '字典键值不能为空', trigger: 'blur' }]
+})
+
+const listClassOptions = ref<DictDataInfo[]>([])
+const statusOptions = ref<DictDataInfo[]>([])
+const dictStore = useDictStore()
+
+const getDictData = async () => {
+  listClassOptions.value = await dictStore.getDictData('sys_style_type')
+  statusOptions.value = await dictStore.getDictData('sys_normal_disable')
+}
+
+const getData = async () => {
+  const dictInfo = await findByIdApi(idsList.value[currentIndex.value])
+  if (dictInfo.code === 200) {
+    formData.value = dictInfo.data
+  }
+}
+
+const emit = defineEmits(['success'])
+const onSubmit = async (formEl: FormInstance | undefined) => {
+  if (!formEl) return
+  await formEl.validate(async (valid, fields) => {
+    if (valid) {
+      const result = await editDictDataApi(formData.value)
+      if (result.code === 200) {
+        ElMessage({
+          message: result.message,
+          type: 'success',
+          plain: true,
+        })
+        emit('success')
+        if (isMultiple.value) {
+          show.value = false
+        } else {
+          currentIndex.value++
+          getData()
+        }
+      } else {
+        ElMessage({
+          message: result.message,
+          type: 'success',
+          plain: true,
+        })
+      }
+    } else {
+      ElMessage({
+        message: Object.values(fields!)[0][0].message,
+        type: 'warning',
+        plain: true,
+      })
+    }
+  })
+}
+
+const formInit = () => {
+  formData.value = {
+    id: '',
+    dictType: '',
+    dictTag: '',
+    dictValue: '',
+    dictOrder: 1,
+    listClass: 'info',
+    status: '0',
+    remark: ''
+  }
+}
+const showModal = (ids: string[]) => {
+  show.value = true
+  formInit()
+  currentIndex.value = 0
+  idsList.value = ids
+  isMultiple.value = idsList.value.length - 1 === currentIndex.value
+  submitText.value = isMultiple.value ? '保存' : '保存并编辑下一页'
+  getData()
+  getDictData()
+}
+
+defineExpose({
+  showModal
+})
+</script>
+
+<style lang="scss" scoped></style>
