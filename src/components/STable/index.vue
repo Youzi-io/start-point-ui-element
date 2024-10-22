cf7a6be372b66d96c96f900aa55826fe6eec4e99
Yuzu <1581523609@qq.com>
2024-10-22 14:04:53 +0800

    feat(STable): 添加 defaultExpandAll 属性
    - 在 STable 组件中新增 defaultExpandAll 属性，用于控制是否默认展开所有行
    - 在 props 中添加 defaultExpandAll 参数，默认值为 false
    - 更新 Props 接口，添加 defaultExpandAll 可选参数

--- src/components/STable/index.vue
+++ src/components/STable/index.vue
@@ -1,7 +1,7 @@
 <template>
   <el-table v-loading="props.loading" :data="props.tableData" style="width: 100%"
-    :header-cell-style="props.headerCellStyle" :row-key="props.rowKey" :border="props.border" default-expand-all
-    @selection-change="handleSelectionChange" @select-all="handleSelectAll">
+    :header-cell-style="props.headerCellStyle" :row-key="props.rowKey" :border="props.border"
+    :default-expand-all="defaultExpandAll" @selection-change="handleSelectionChange" @select-all="handleSelectAll">
     <el-table-column v-for="item in props.columns" :key="item.prop" :type="item.type" :prop="item.prop"
       :label="item.label" :width="item.width" :min-width="item.minWidth" :align="item.align"
       :show-overflow-tooltip="item.showOverflowTooltip">
@@ -17,7 +17,8 @@ import type { Columns } from '@/types/components/sTable';
 
 const props = withDefaults(defineProps<Props<T>>(), {
   rowKey: 'id',
-  border: true
+  border: true,
+  defaultExpandAll: false
 })
 
 const emit = defineEmits(['selectionChange', 'selectAll'])
@@ -38,6 +39,7 @@ interface Props<T> {
   loading?: boolean;
   rowKey?: string;
   border?: boolean
+  defaultExpandAll?: boolean
 }
 </script>
 
